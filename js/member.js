1ae4bf4a359fcd2a2e4158d7c7cddbb85c43c5d6
cuiruilong <crl_228@163.com>
2019-01-22 11:43:01 +0800

    增加真实头像姓名

--- js/member.js
+++ js/member.js
@@ -1,162 +1,177 @@
 var member = [
   {
     "phone": "0001",
-    "name": "小瓶盖"
+    "name": "Chris",
+    "avatar": "https://web-cdn.aiyoujia.com/lottery/Chris.jpg"
   },
   {
     "phone": "0002",
-    "name": "小瓶盖"
+    "name": "贝云江",
+    "avatar": "https://web-cdn.aiyoujia.com/lottery/贝云江.jpg"
   },
   {
     "phone": "0003",
-    "name": "小瓶盖"
+    "name": "陈孝宁",
+    "avatar": "https://web-cdn.aiyoujia.com/lottery/陈孝宁.jpg"
   },
   {
     "phone": "0004",
-    "name": "小瓶盖"
+    "name": "陈雨生",
+    "avatar": "https://web-cdn.aiyoujia.com/lottery/陈雨生.jpg"
   },
   {
     "phone": "0005",
-    "name": "小瓶盖"
+    "name": "仇晓芬",
+    "avatar": "https://web-cdn.aiyoujia.com/lottery/仇晓芬.jpg"
   },
   {
     "phone": "0006",
-    "name": "小瓶盖"
+    "name": "丹丹",
+    "avatar": "https://web-cdn.aiyoujia.com/lottery/丹丹.jpg"
   },
   {
     "phone": "0007",
-    "name": "小瓶盖"
+    "name": "胡晨飞",
+    "avatar": "https://web-cdn.aiyoujia.com/lottery/胡晨飞.jpg"
   },
   {
     "phone": "0008",
-    "name": "小瓶盖"
+    "name": "胡南平",
+    "avatar": "https://web-cdn.aiyoujia.com/lottery/胡南平.jpg"
   },
   {
     "phone": "0009",
-    "name": "小瓶盖"
+    "name": "胡学良",
+    "avatar": "https://web-cdn.aiyoujia.com/lottery/胡学良.jpg"
   },
   {
     "phone": "0010",
-    "name": "小瓶盖"
+    "name": "蒋静",
+    "avatar": "https://web-cdn.aiyoujia.com/lottery/蒋静.jpg"
   },
   {
     "phone": "0011",
-    "name": "小瓶盖"
+    "name": "老司机",
+    "avatar": "https://web-cdn.aiyoujia.com/lottery/老司机.jpg"
   },
   {
     "phone": "0012",
-    "name": "小瓶盖"
+    "name": "磊爷",
+    "avatar": "https://web-cdn.aiyoujia.com/lottery/磊爷.jpg"
   },
   {
     "phone": "0013",
-    "name": "小瓶盖"
+    "name": "李俱顺",
+    "avatar": "https://web-cdn.aiyoujia.com/lottery/李俱顺.jpg"
   },
   {
     "phone": "0014",
-    "name": "小瓶盖"
+    "name": "李平平",
+    "avatar": "https://web-cdn.aiyoujia.com/lottery/李平平.jpg"
   },
   {
     "phone": "0015",
-    "name": "小瓶盖"
+    "name": "李鑫",
+    "avatar": "https://web-cdn.aiyoujia.com/lottery/李鑫.jpg"
   },
   {
     "phone": "0016",
-    "name": "小瓶盖"
+    "name": "李勇达",
+    "avatar": "https://web-cdn.aiyoujia.com/lottery/李勇达.jpg"
   },
   {
     "phone": "0017",
-    "name": "小瓶盖"
+    "name": "刘子明",
+    "avatar": "https://web-cdn.aiyoujia.com/lottery/刘子明.jpg"
   },
   {
     "phone": "0018",
-    "name": "小瓶盖"
+    "name": "陆灿章",
+    "avatar": "https://web-cdn.aiyoujia.com/lottery/陆灿章.jpg"
   },
   {
     "phone": "0019",
-    "name": "小瓶盖"
+    "name": "陆超",
+    "avatar": "https://web-cdn.aiyoujia.com/lottery/陆超.jpg"
   },
   {
     "phone": "0020",
-    "name": "小瓶盖"
+    "name": "吕振华",
+    "avatar": "https://web-cdn.aiyoujia.com/lottery/吕振华.jpg"
   },
   {
     "phone": "0021",
-    "name": "小瓶盖"
+    "name": "潘旭园",
+    "avatar": "https://web-cdn.aiyoujia.com/lottery/潘旭园.jpg"
   },
   {
     "phone": "0022",
-    "name": "小瓶盖"
+    "name": "王成旺",
+    "avatar": "https://web-cdn.aiyoujia.com/lottery/王成旺.jpg"
   },
   {
     "phone": "0023",
-    "name": "小瓶盖"
+    "name": "王紫鹃",
+    "avatar": "https://web-cdn.aiyoujia.com/lottery/王紫鹃.jpg"
   },
   {
     "phone": "0024",
-    "name": "小瓶盖"
+    "name": "文明",
+    "avatar": "https://web-cdn.aiyoujia.com/lottery/文明.jpg"
   },
   {
     "phone": "0025",
-    "name": "小瓶盖"
+    "name": "小满",
+    "avatar": "https://web-cdn.aiyoujia.com/lottery/小满.jpg"
   },
   {
     "phone": "0026",
-    "name": "小瓶盖"
+    "name": "小瓶盖",
+    "avatar": "https://web-cdn.aiyoujia.com/lottery/小瓶盖.jpg"
   },
   {
     "phone": "0027",
-    "name": "小瓶盖"
+    "name": "小雨点",
+    "avatar": "https://web-cdn.aiyoujia.com/lottery/小雨点.jpg"
   },
   {
     "phone": "0028",
-    "name": "小瓶盖"
+    "name": "徐宏",
+    "avatar": "https://web-cdn.aiyoujia.com/lottery/徐宏.jpg"
   },
   {
     "phone": "0029",
-    "name": "小瓶盖"
+    "name": "徐倩",
+    "avatar": "https://web-cdn.aiyoujia.com/lottery/徐倩.jpg"
   },
   {
     "phone": "0030",
-    "name": "小瓶盖"
+    "name": "许植",
+    "avatar": "https://web-cdn.aiyoujia.com/lottery/许植.jpg"
   },
   {
     "phone": "0031",
-    "name": "小瓶盖"
+    "name": "亚卿",
+    "avatar": "https://web-cdn.aiyoujia.com/lottery/亚卿.jpg"
   },
   {
     "phone": "0032",
-    "name": "小瓶盖"
+    "name": "裕仁",
+    "avatar": "https://web-cdn.aiyoujia.com/lottery/裕仁.jpg"
   },
   {
     "phone": "0033",
-    "name": "小瓶盖"
+    "name": "翟宇航",
+    "avatar": "https://web-cdn.aiyoujia.com/lottery/翟宇航.jpg"
   },
   {
     "phone": "0034",
-    "name": "小瓶盖"
+    "name": "张路",
+    "avatar": "https://web-cdn.aiyoujia.com/lottery/张路.jpg"
   },
   {
     "phone": "0035",
-    "name": "小瓶盖"
-  },
-  {
-    "phone": "0036",
-    "name": "小瓶盖"
-  },
-  {
-    "phone": "0037",
-    "name": "小瓶盖"
-  },
-  {
-    "phone": "0038",
-    "name": "小瓶盖"
-  },
-  {
-    "phone": "0039",
-    "name": "小瓶盖"
-  },
-  {
-    "phone": "0040",
-    "name": "小瓶盖"
+    "name": "兆振",
+    "avatar": "https://web-cdn.aiyoujia.com/lottery/兆振.jpg"
   }
 ];
